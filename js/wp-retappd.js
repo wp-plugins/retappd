ef343109e19f1a9adaac5636dbd5f09214f6677f
jaydensibert <jaydensibert@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-12 18:54:43 +0000

    Untappd will be discontinuing our standard HTTP API endpoint on 2/1/15. This plugin was updated to use their new HTTPS endpoint.
    Added the ability to paginate results which can be enabled and disabled.
    
    Added the ability to control the css display of the pagination button when pagination is enabled.
    
    git-svn-id: https://plugins.svn.wordpress.org/retappd/trunk@1066282 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/wp-retappd.js
+++ js/wp-retappd.js
@@ -15,4 +15,23 @@ jQuery(document).ready(function($) {
   $('#retappd-display-brewery-no').click(function () {
 	$("#brewery_country").hide();
   });
+  
+  // check if the user has enabled pagination
+  if($('#retappd-display-pagination-yes').is(':checked')) { 
+	$("#pagination_settings").show();
+  } else {
+	$("#pagination_settings").hide();
+  }
+  
+  // show the pagination settings
+  $('#retappd-display-pagination-yes').click(function () {
+	$("#pagination_settings").show();
+  });
+  
+  // hide the pagination settings
+  $('#retappd-display-pagination-no').click(function () {
+	$("#pagination_settings").hide();
+  });
+  
+ 
 });
